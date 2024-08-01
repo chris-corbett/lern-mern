eab7ad74713f1715f44d6bcf4d36252b9070afb5
chris-corbett <corbett.chris1915@gmail.com>
2024-08-01 01:46:08 -0400

    Now connects to mongodb database

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 const projectsRouter = require("./routes/projects");
@@ -15,8 +16,16 @@ app.use((req: any, res: any, next: any) => {
 // Routes
 app.use("/api/projects", projectsRouter);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+// Database connection
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((error: any) => {
+        console.log(error);
+    });
 
 export {}; // This is needed to avoid a TypeScript error
