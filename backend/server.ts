85725ca4d8f663b55d8ff5587df8caf703235f43
chris-corbett <corbett.chris1915@gmail.com>
2024-08-02 13:05:48 -0400

    cors now used (Enable All CORS Requests)

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 
 const projectRouter = require("./routes/ProjectRouter");
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use((req: any, res: any, next: any) => {
     console.log(req.path, req.method);
